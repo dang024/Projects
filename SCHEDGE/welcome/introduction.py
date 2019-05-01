f988915761ecca11a4a7ed2539c7679890ec30e2
dang024 <you@example.com>
2019-05-01 14:15:29 -0400

    welcome!

---
+++ SCHEDGE/welcome/introduction.py
@@ -0,0 +1,3 @@
+print ("Welcome to Schedge. The last scheduling app you will ever need (:")
+# Figure out how to do letters coming out in a cool, soft way
+# This should prob be really aesthetically pleasing
